fe7db6dd0b5ad9dd1927d8421d7aa989c8577670
micronyks <micronyks@github.com>
2022-03-21 15:07:32 +0530

    first cmt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useLayoutEffect, useState } from 'react';
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// custom imports
+import HeaderComponent from './components/header.component';
+
+
+const App: React.FC<{ history: any, navigateTo: (routeTo: string) => void, profileMenuClickHandler:(menuItem:string)=>void, isAuthenticated:boolean }> = (props) => {
+
+  console.log('header_App_component', props);
+
+  const [state, setState] = useState({
+    action: props?.history?.action,
+    location: props?.history?.location
+  })
+
+  useLayoutEffect(() => props?.history.listen(setState), [props?.history])
+
+  return <HeaderComponent navigateTo={props.navigateTo} profileMenuClickHandler={props.profileMenuClickHandler}/>
 }
 
 export default App;
